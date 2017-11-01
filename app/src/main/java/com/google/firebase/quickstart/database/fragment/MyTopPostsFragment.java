77b84b17bfe7fb3c32be744657471b47dd669eb5
jinhaotang <jinhao.tang@hotmail.com>
2017-11-01 14:23:01 -0700

    Initial commit

---
+++ app/src/main/java/com/google/firebase/quickstart/database/fragment/MyTopPostsFragment.java
@@ -0,0 +1,24 @@
+package com.google.firebase.quickstart.database.fragment;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
+public class MyTopPostsFragment extends PostListFragment {
+
+    public MyTopPostsFragment() {}
+
+    @Override
+    public Query getQuery(DatabaseReference databaseReference) {
+        // [START my_top_posts_query]
+        // My top posts by number of stars
+        String myUserId = getUid();
+
+
+        // this is actually the command from firebase
+        Query myTopPostsQuery = databaseReference.child("user-posts").child(myUserId)
+                .orderByChild("starCount");
+        // [END my_top_posts_query]
+
+        return myTopPostsQuery;
+    }
+}
